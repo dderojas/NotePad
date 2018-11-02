af63999faf65622d3496ca5860c802d2d3a90c6d
Don de Rojas <derojasdonny@gmail.com>
2018-11-01 18:21:44 -0700

    add app component, index.jsx and start the server folder

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>NotePad with PostgreSQL</h1>
+      </div>
+    )
+  }
+}
+
+export
