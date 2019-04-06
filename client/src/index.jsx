04b3f1da04130e31d8bbf194b74e95cf76a9a91f
Don de Rojas <derojasdonny@gmail.com>
2019-04-05 17:57:47 -0700

    install react-redux, redux, redux-think, set up Provider tag in index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
 import App from './components/App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+var store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('app'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('app'));
